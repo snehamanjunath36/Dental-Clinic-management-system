1a62886dcce6e86788e31e5fe7cfb2c72323cf5a
sneha <smgsneha14@gmail.com>
2022-07-26 20:13:13 +0530

    removed trial code

--- home_page.php
+++ home_page.php
@@ -9,58 +9,6 @@ session_start();
             DENTAL CLINIC
         </title>
     </head>
-<!--  <style> 
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-   img{ height: 400px;
-    width: auto;
-    
-   }
-   body{
-       width: 100%;
-      justify-content: center;
-      display: flex;
-
-   }
- table td a{
-   border: 1000px;
-   text-decoration: none;
-   }
-   table td h3{
-    justify-content: center;
-    align-items: center;
-    font-size: medium;
-    font-weight: 400;
-   
-   }
-   table tr{
-    justify-content: center;
-    align-items: center;
-   }
-   table{
-       border-collapse: collapse;
-      
-   }
-   td button{
-       border:none;
-       padding: 7px 20px;
-       border-radius: 20px;
-       background-color: black;
-       color: white;
-   }
-   h1{
-       font-size:40px;
-    
-   }
-   body,h1{
-       background-color: Lavender;
-       justify-content: center;
-    
-   }
-</style> -->
 
     <h1>WELCOME TO DENTAL CLINIC HOME PAGE</h1><HR SIZE="3"  COLOR="BLUE">
     <br>
